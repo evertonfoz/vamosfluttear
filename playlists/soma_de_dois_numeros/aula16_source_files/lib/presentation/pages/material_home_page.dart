527c64fb42fe97b4556b46e8d400b0a90cc84174
evertonfoz <evertoncoimbra@gmail.com>
2021-03-05 11:16:29 -0300

    aula 17

--- playlists/soma_de_dois_numeros/aula16_source_files/lib/presentation/pages/material_home_page.dart
+++ playlists/soma_de_dois_numeros/aula16_source_files/lib/presentation/pages/material_home_page.dart
@@ -3,8 +3,57 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:soma_de_dois_valores/presentation/pages/first_value_page.dart';
 import 'package:soma_de_dois_valores/presentation/widgets/hero_calculator_widget.dart';
 
-class MaterialHomePage extends StatelessWidget {
+class MaterialHomePage extends StatefulWidget {
   static const routeName = '/';
+
+  @override
+  _MaterialHomePageState createState() => _MaterialHomePageState();
+}
+
+class _MaterialHomePageState extends State<MaterialHomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _animation;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(
+        seconds: 2,
+      ),
+    );
+
+    _animation = Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(_controller);
+
+    _controller.repeat(
+      // min: 0.0,
+      // max: 1.0,
+      reverse: true,
+      // period: Duration(
+      //   seconds: 10,
+      // ),
+    );
+
+    // _controller.forward();
+    //
+    // _animation.addStatusListener((status) {
+    //   if (status == AnimationStatus.completed) {
+    //     _controller.reverse();
+    //   } else if (status == AnimationStatus.dismissed) {
+    //     _controller.forward();
+    //   }
+    // });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,12 +88,15 @@ class MaterialHomePage extends StatelessWidget {
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          Text(
-            'Comece por aqui',
-            style: GoogleFonts.aBeeZee(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.indigo),
+          FadeTransition(
+            opacity: _animation,
+            child: Text(
+              'Comece por aqui',
+              style: GoogleFonts.aBeeZee(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.indigo),
+            ),
           ),
           SizedBox(
             width: 5,
