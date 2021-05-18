2eb46e6fa272952e083d1d6b09fbe5c557cd15d2
evertonfoz <evertoncoimbra@gmail.com>
2021-05-18 10:33:40 -0300

    1033

--- playlists/introducao_ao_dart_e_flutter/aula_03/android/app/src/main/kotlin/br/com/evertoncoimbra/aula_03/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package br.com.evertoncoimbra.aula_03
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- playlists/introducao_ao_dart_e_flutter/aula_03/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'core/app.dart';
-
-void main() {
-  runApp(App());
-}
